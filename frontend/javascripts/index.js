677b728ab76eb4df7ee1b0f94ac8ca057b7ae5e4
SeoBYP <unialgame@gmail.com>
2024-12-07 01:45:02 +0900

    로그인, 회원가입 API 구현

--- frontend/javascripts/index.js
+++ frontend/javascripts/index.js
@@ -4,9 +4,8 @@ const onClickGameListItem = (event) => {
     window.location.pathname = '/playgame.html'; // 올바른 절대 경로 사용
 }
 
-function updateGameList(){
+async function renderGames (data){
     const gameList = document.querySelector(".game-list");
-
     for(let i = 0; i < 10; i++)
     {
         const li = document.createElement('li');
@@ -17,7 +16,22 @@ function updateGameList(){
     }
 }
 
-
+async function updateGameList(){
+    const accessToken = window.localStorage.getItem('token');
+    const res = await fetch('/games',{
+        headers:{
+            'Authorization': `Bearer ${accessToken}`
+        }
+    });
+    if(res.status === 200){
+        const gameData = await res.json();
+        renderGames(gameData)
+    }
+    else {
+        alert("로그인이 필요합니다.")
+        window.location.pathname = '/login.html';
+    }
+}
 
 updateGameList();
 
